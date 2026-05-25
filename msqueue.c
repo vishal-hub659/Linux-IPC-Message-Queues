92100118d67d5a507cdb37e541325d55cff32ee5
vishal <vishal@localhost.localdomain>
2026-05-25 17:45:55 +0200

    success

--- msqueue.c
+++ msqueue.c
@@ -1,28 +1,3 @@
-# Linux-IPC-Message-Queues
-Linux IPC-Message Queues
-
-# AIM:
-To write a C program that receives a message from message queue and display them
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux message queues API 
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## C program that receives a message from message queue and display them
-
-```c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -101,14 +76,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-```
-
-
-
-## OUTPUT
-
-
-![Alt text](msqueue.png)
-
-# RESULT:
-The programs are executed successfully.
